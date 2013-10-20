1357b83785ce777b750ff4fcadd8070f487b97ea
TheLightBird <thelightbird@gmail.com>
2013-10-20 14:05:24 +0200

    Added FOSUserBundle

---
+++ src/CoralScrum/UserBundle/CoralScrumUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CoralScrum\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoralScrumUserBundle extends Bundle
+{
+}
---
+++ src/CoralScrum/UserBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CoralScrum\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="CoralScrum\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
