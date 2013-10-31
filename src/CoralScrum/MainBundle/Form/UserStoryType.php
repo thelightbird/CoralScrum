2ab3a42787eec832edbc08c06c7ce287a250686b
TheLightBird <thelightbird@gmail.com>
2013-10-31 15:43:32 +0100

    Added Fibonacci numbers to select difficulty and priority in User Stories

--- src/CoralScrum/MainBundle/Form/UserStoryType.php
+++ src/CoralScrum/MainBundle/Form/UserStoryType.php
@@ -14,14 +14,19 @@ class UserStoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $fibonacci = array(
+            '0' => 0, '1' => 1, '1' => 1, '2' => 2, '3' => 3, '5' => 5, '8' => 8, 
+            '13' => 13, '21' => 21, '34' => 34, '55' => 55, '89' => 89, '144' => 144, 
+            '233' => 233, '377' => 377, '610' => 610, '987' => 987
+        );
         $builder
             ->add('title')
             ->add('description')
-            ->add('priority', 'integer', array(
-                'attr' => array('min' => 0)
+            ->add('priority', 'choice', array(
+                'choices' => $fibonacci,
             ))
-            ->add('difficulty', 'integer', array(
-                'attr' => array('min' => 0)
+            ->add('difficulty', 'choice', array(
+                'choices' => $fibonacci,
             ))
             //->add('isFinished')
             //->add('isValidated')
